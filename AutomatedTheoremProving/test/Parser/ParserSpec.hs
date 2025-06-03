5651356b89e0d2b9e7e4d1a8479d44dbee2f9c52
Aleksandra Klemczak <01171382@pw.edu.pl>
2025-06-03 11:24:35 +0200

    formula generator

--- AutomatedTheoremProving/test/Parser/ParserSpec.hs
+++ AutomatedTheoremProving/test/Parser/ParserSpec.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Parser.ParserSpec (tests) where
 
 import Test.Framework (testGroup)
@@ -36,17 +37,16 @@ parseTest parser input expected =
 test_axiomStmt :: Assertion
 test_axiomStmt = parseTest parseProgram "AXIOM MyAxiom : (A ∧ B);" (Program [Axiom "MyAxiom" (And (Var "A") (Var "B"))])
 
-
-test_theorem :: Assertion 
+test_theorem :: Assertion
 test_theorem = parseTest parseProgram "THEOREM simple_implication: (P → Q) → (¬Q → ¬P);" (Program [Theorem "simple_implication" (Implies (Implies (Var "P") (Var "Q")) (Implies (Not (Var "Q")) (Not (Var "P"))))])
 
-test_given :: Assertion 
+test_given :: Assertion
 test_given = parseTest parseProgram "GIVEN P → Q;" (Program [Given (Implies (Var "P") (Var "Q"))])
 
 test_assertStmt :: Assertion
 test_assertStmt = parseTest parseProgram "ASSERT A ∨ B;" (Program [Assert (Or (Var "A") (Var "B"))])
 
-test_applyStmt :: Assertion 
+test_applyStmt :: Assertion
 test_applyStmt = parseTest parseProgram "APPLY contrapositive TO (P → Q) GET (¬Q → ¬P);" (Program [Apply "contrapositive" [Implies (Var "P") (Var "Q"), Implies (Not (Var "Q")) (Not (Var "P"))]])
 
 test_byContradictionStmt :: Assertion
@@ -54,3 +54,31 @@ test_byContradictionStmt = parseTest parseProgram "BY CONTRADICTION { AXIOM MyAx
 
 test_qedStmt :: Assertion
 test_qedStmt = parseTest parseProgram "QED;" (Program [Qed])
+
+genIdentifier :: Gen String
+genIdentifier = do
+    first <- elements $ ['a' .. 'z']
+    rest <- listOf $ elements $ ['a' .. 'z'] ++ ['A' .. 'Z'] ++ ['0' .. '9'] ++ ['_']
+    return (first : take 10 rest)
+
+genBool :: Gen Bool
+genBool = elements [True, False]
+
+genFormula :: Int -> Gen Formula
+genFormula 0 =
+    oneof
+        [ Var <$> genIdentifier
+        , Const <$> genBool
+        ]
+genFormula n
+    | n > 0 =
+        let subformula = genFormula (n - 1)
+         in frequency
+                [ (4, Var <$> genIdentifier)
+                , (3, Not <$> subformula)
+                , (2, And <$> subformula <*> subformula)
+                , (2, Or <$> subformula <*> subformula)
+                , (1, Implies <$> subformula <*> subformula)
+                , (1, Equivalent <$> subformula <*> subformula)
+                , (1, Const <$> genBool)
+                ]
